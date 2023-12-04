68990740433c828ee37e7c60cdc257ee7036168e
Natt P <nattster@gmail.com>
2023-12-05 00:00:58 +0700

    part 1

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,30 +1,36 @@
+import java.math.BigInteger
+
 fun main() {
 
-    fun cardValue(line: String): Int {
-        val card = line.split(" ")[1]
+    fun cardValue(line: String): BigInteger {
+        val card = line.split(":")[1]
         val winningHave = card.split("|")
-        val winning = winningHave[0].trim().split(" ").toSet()
-        val score = winningHave[1].trim().split(" ").toList().map {
+        val winning = winningHave[0].trim().split("\\s+".toRegex()).toSet()
+        val l = winningHave[1].trim().split("\\s+".toRegex()).toList()
+            val score = l.map {
             if (winning.contains(it)) {
                 1
             } else {
                 0
             }
         }.sum()
-        return 1 shl score
+        if (score == 0) {
+            return BigInteger.ZERO
+        }
+        return BigInteger.valueOf(2L).pow(score - 1)
 
     }
 
-    fun part1(input: List<String>): Int {
+    fun part1(input: List<String>): BigInteger {
         return input.sumOf { cardValue(it) }
     }
 
     // test if implementation meets criteria from the description, like:/**/
     val testInput = readInput("Day04_test")
-    check(part1(testInput) == 13)
+    check(part1(testInput) == BigInteger.valueOf(13L))
 //    check(part2(testInput) == 467835)
 //
-//    val input = readInput("Day03")
-//    part1(input).println()
+    val input = readInput("Day04")
+    part1(input).println()
 //    part2(input).println()
 }
