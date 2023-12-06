b53a34154217532f76a894521abd48edae127e04
Natt P <nattster@gmail.com>
2023-12-07 00:51:49 +0700

    day 6

--- src/Day06.kt
+++ src/Day06.kt
@@ -3,41 +3,42 @@ import kotlin.math.max
 import kotlin.math.min
 
 fun main() {
-    fun parseNumbers(line: String): List<Int> {
-        return line.trim().split("\\s+".toRegex()).map { it.toInt() }
+    fun parseNumbers(line: String): List<Long> {
+        return line.trim().split("\\s+".toRegex()).map { it.toLong() }
     }
 
-    fun findWays(time: Int, recordDistance: Int): Int {
-        var count = 0
+    fun findWays(time: Long, recordDistance: Long): Long {
+        var count = 0L
         for (hold in 0..time) {
             val distance = (time - hold) * hold
             if (distance > recordDistance) {
-                count += 1
+                count += 1L
             }
         }
         return count
     }
 
+    fun solve(times: List<Long>, distances: List<Long>): Long =
+        times.zip(distances).map { findWays(it.first, it.second) }.reduce { acc, i -> acc * i }
+
     fun part1(input: List<String>): Long {
         val times = parseNumbers(input[0].split(":")[1])
         val distances = parseNumbers(input[1].split(":")[1])
-        var result = 1L
-        for (it in times.zip(distances)) {
-            result *= findWays(it.first, it.second)
-        }
-        return result
+        return solve(times, distances)
     }
 
     fun part2(input: List<String>): Long {
-        return 0
+        val times = parseNumbers(input[0].split(":")[1].replace(" ", ""))
+        val distances = parseNumbers(input[1].split(":")[1].replace(" ", ""))
+        return solve(times, distances)
     }
 
     // test if implementation meets criteria from the description, like:/**/
     val testInput = readInput("Day06_test")
     check(part1(testInput) == 288L)
-//    check(part2(testInput) == 46L)
-//
+    check(part2(testInput) == 71503L)
+
     val input = readInput("Day06")
     part1(input).println()
-//    part2(input).println()
+    part2(input).println()
 }
